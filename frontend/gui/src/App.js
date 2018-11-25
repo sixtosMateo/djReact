7ec856fd2275e2ded4f4d65cd26d6e3a45d3faf5
mateosixtos <mateo@nlightntech.com>
2018-11-24 20:05:36 -0800

    initial routes and ArticleDetailView that display content from django server

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import BaseRouter from './routes';
 
 import 'antd/dist/antd.css';
 
 import CustomLayout from './containers/Layout';
-import ArticleList from './containers/ArticleListView'
+
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <Router>
         <CustomLayout>
-          <ArticleList/>
+          <BaseRouter/>
         </CustomLayout>
+        </Router>
       </div>
     );
   }
