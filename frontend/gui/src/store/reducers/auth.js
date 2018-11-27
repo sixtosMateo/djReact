a66b69224dc01555ab41bfba2f82fa84f35e1bd4
mateosixtos <mateosixtos@gmail.com>
2018-11-26 22:22:18 -0800

    initializing reducers/auth.js file

--- frontend/gui/src/store/reducers/auth.js
+++ frontend/gui/src/store/reducers/auth.js
@@ -1,1 +1,15 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
+// here we define our initial state
+    // ex: loading any errors, authentication token
+
+
+const initialState = {
+   token: null,
+   error: null,
+   loading: false
+}
+
+
+// recreating the methods that go hand to hand with our actions
