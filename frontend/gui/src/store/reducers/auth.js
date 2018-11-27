341c4431d94263ba67213820ba46e9559640c0db
mateosixtos <mateosixtos@gmail.com>
2018-11-26 22:28:47 -0800

    stating the authStart at reducers using utily/updateObject method

--- frontend/gui/src/store/reducers/auth.js
+++ frontend/gui/src/store/reducers/auth.js
@@ -13,3 +13,13 @@ const initialState = {
 
 
 // recreating the methods that go hand to hand with our actions
+
+// must return state that is updated
+const authStart = (state, action) => {
+    // what updateObject() method is doing is when we start we set
+      // error to null and loading to true -> spinner will start spinning
+    return updateObject(state,{
+      error: null,
+      loading: true
+    })
+}
