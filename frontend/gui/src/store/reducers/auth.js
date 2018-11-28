5a1c44c5e55a02c31fe9c7b6abef8a995a9f2d6d
mateosixtos <mateosixtos@gmail.com>
2018-11-27 23:10:37 -0800

    defining method where it takes place

--- frontend/gui/src/store/reducers/auth.js
+++ frontend/gui/src/store/reducers/auth.js
@@ -46,3 +46,15 @@ const authLogout = (state, action) => {
       token: null
     })
 }
+
+//define the methods where they take place
+const reducer = (state=initialState, action) ={
+  switch (action.type) {
+      case actionTypes.AUTH_START: return authStart(state, action);
+      case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+      case actionTypes.AUTH_FAIL: return authFail(state, action);
+      case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
+      default:
+        return state;
+  }
+}
