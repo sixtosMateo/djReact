0991b4f61047ba6792e6466e08864855c2c687a1
mateosixtos <mateosixtos@gmail.com>
2018-11-29 23:34:58 -0800

    installing rest_auth. restframework and makes it into an API. Takes all django login methods oauth

--- backend/src/djreact/urls.py
+++ backend/src/djreact/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('articles.api.urls'))
+    path('api/', include('articles.api.urls')),
+    path('rest-auth/', include('rest_auth.urls'))
+
 ]
